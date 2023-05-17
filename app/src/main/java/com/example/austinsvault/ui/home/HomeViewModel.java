9341df18b1ae4a43fee8a4156135d717e16bb992
jalowe13 <jplowe21@vt.edu>
2023-05-17 16:18:13 -0400

    0.1 - Basic Menu Templating
    Base to start the app functionality off from

--- app/src/main/java/com/example/austinsvault/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/austinsvault/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Welcome to Austins Vault\n \n To make a new character press the + button");
     }
 
     public LiveData<String> getText() {
